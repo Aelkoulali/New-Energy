d25f0d429e9a2d57efa031929b7ee31bfc47124e
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-11-24 21:52:57 +0100

    Make the image stretch all the way across the screen

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Make the image stretch all the way across the screen
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
